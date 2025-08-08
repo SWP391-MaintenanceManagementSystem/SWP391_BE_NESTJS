f75469a0cf244b008d0e022e7d55333babb995a6
unknown <lamtailoi11141@gmail.com>
2025-08-09 00:16:11 +0700

    [LoiLT] feat: add Serialize Interceptor

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,6 +6,8 @@ import { LocalAuthGuard } from './guard/local-auth.guard';
 import { Response, Request } from 'express';
 import { Account } from '@prisma/client';
 import { Public } from './decorator/public';
+import { Serialize } from 'src/interceptor/serialize.interceptor';
+import { AccountDTO } from 'src/account/dto/account.dto';
 export interface RequestWithUser extends Request {
   user: Account;
 }
@@ -15,6 +17,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Public()
+  @Serialize(AccountDTO)
   @Post('/signup')
   async signUp(@Body() body: SignUpDTO) {
     return await this.authService.signUp(body);
