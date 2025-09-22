57f4fb7dac7017b53dc722ac471010f446d03c4c
NT4105 <tunguyen04105@gmail.com>
2025-09-22 23:44:00 +0700

    [TuNM] feat: create CRUD api for certificate

---
+++ src/modules/employee/certificate/certificate.controller.ts
@@ -0,0 +1,58 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CertificateService } from './certificate.service';
+import { CreateCertificateDto } from './dto/create-certificate.dto';
+import { UpdateCertificateDto } from './dto/update-certificate.dto';
+import { ApiTags, ApiBearerAuth, ApiBody } from '@nestjs/swagger';
+import { Roles } from 'src/common/decorator/role.decorator';
+import { AccountRole } from '@prisma/client';
+
+@ApiTags('Certificates')
+@Controller('certificates')
+@ApiBearerAuth()
+@Roles(AccountRole.ADMIN)
+export class CertificateController {
+  constructor(private readonly certificateService: CertificateService) {}
+
+  @Post('/')
+  @Roles(AccountRole.ADMIN)
+  @ApiBody({ type: CreateCertificateDto })
+  async createCertificate(@Body() createCertificateDto: CreateCertificateDto) {
+    await this.certificateService.createCertificate(createCertificateDto);
+    return { message: 'Certificate created successfully' };
+  }
+
+  @Get('/employee/:employeeId')
+  @Roles(AccountRole.ADMIN)
+  async getCertificatesByEmployeeId(@Param('employeeId') employeeId: string) {
+    await this.certificateService.getCertificateByEmployeeId(employeeId);
+    return { message: `Certificates for employeeId ${employeeId} retrieved successfully` };
+  }
+
+  @Get('/:id')
+  @Roles(AccountRole.ADMIN)
+  @ApiBearerAuth('jwt-auth')
+  async getCertificateById(@Param('id') id: string) {
+    await this.certificateService.getCertificateById(id);
+    return { message: `Certificate with id ${id} retrieved successfully` };
+  }
+
+  @Put('/:id')
+  @Roles(AccountRole.ADMIN)
+  @ApiBearerAuth('jwt-auth')
+  @ApiBody({ type: UpdateCertificateDto })
+  async updateCertificate(
+    @Param('id') id: string,
+    @Body() updateCertificateDto: UpdateCertificateDto
+  ) {
+    await this.certificateService.updateCertificate(id, updateCertificateDto);
+    return { message: `Certificate updated successfully` };
+  }
+
+  @Delete('/:id')
+  @Roles(AccountRole.ADMIN)
+  @ApiBearerAuth('jwt-auth')
+  async deleteCertificate(@Param('id') id: string) {
+    await this.certificateService.deleteCertificate(id);
+    return { message: `Certificate deleted successfully` };
+  }
+}
--- src/modules/employee/dto/employee.dto.ts
+++ src/modules/employee/dto/employee.dto.ts
@@ -1,27 +1,27 @@
-import { Expose, Transform } from "class-transformer";
+import { Expose, Transform } from 'class-transformer';
 
 export class EmployeeDTO {
-    @Expose()
-    firstName: string;
+  @Expose()
+  firstName: string;
 
-    @Expose()
-    lastName: string;
+  @Expose()
+  lastName: string;
 
-    @Expose()
-    specialization?: string;
+  @Expose()
+  specialization?: string;
 
-    @Expose()
-    experienceYears?: number;
+  @Expose()
+  experienceYears?: number;
 
-    // TODO: make certificate DTO
-    @Expose()
-    certificate?: string;
+  // TODO: make certificate DTO
+  @Expose()
+  certificate?: string;
 
-    @Expose()
-    @Transform(({ value }) => (value instanceof Date ? value.toISOString() : value))
-    createdAt: Date;
+  @Expose()
+  @Transform(({ value }) => (value instanceof Date ? value.toISOString() : value))
+  createdAt: Date;
 
-    @Expose()
-    @Transform(({ value }) => (value instanceof Date ? value.toISOString() : value))
-    updatedAt: Date;
+  @Expose()
+  @Transform(({ value }) => (value instanceof Date ? value.toISOString() : value))
+  updatedAt: Date;
 }
