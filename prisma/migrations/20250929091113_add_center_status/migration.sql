a5660474adae927230fecc7e0ed13094fa8f98d2
lamtailoi2 <lamtailoi11141@gmail.com>
2025-10-03 16:15:25 +0700

    [LoiLT] hotfix: remove old migration

--- prisma/migrations/20250929091113_add_center_status/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "public"."CenterStatus" AS ENUM ('OPEN', 'CLOSED');
-
--- AlterTable
-ALTER TABLE "public"."service_centers" ADD COLUMN     "status" "public"."CenterStatus" NOT NULL DEFAULT 'OPEN';
