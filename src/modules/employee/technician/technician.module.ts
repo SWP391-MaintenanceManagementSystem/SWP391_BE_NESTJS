37d10f16191123e835a4422eb3adba79b7f20c67
NT4105 <tunguyen04105@gmail.com>
2025-10-08 01:45:38 +0700

    [TuNM] feat: add import

--- src/modules/employee/technician/technician.module.ts
+++ src/modules/employee/technician/technician.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TechnicianService } from './technician.service';
 import { TechnicianController } from './technician.controller';
 import { AccountModule } from 'src/modules/account/account.module';
+import { EmployeeModule } from '../employee.module';
 
 @Module({
-  imports: [AccountModule],
+  imports: [AccountModule, EmployeeModule],
   controllers: [TechnicianController],
   providers: [TechnicianService],
   exports: [TechnicianService],
