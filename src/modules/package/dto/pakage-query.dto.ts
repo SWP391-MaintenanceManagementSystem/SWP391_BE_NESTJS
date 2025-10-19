3809cab4e5a4792669b5351f72b0b5bcdb6d0f01
LapMD <nekor210@gmail.com>
2025-10-19 19:16:57 +0700

    [LapMD] fix: update Pakage module

---
+++ src/modules/package/dto/pakage-query.dto.ts
@@ -0,0 +1,44 @@
+import { ApiPropertyOptional } from "@nestjs/swagger";
+import { PackageStatus } from "@prisma/client";
+import { Type } from "class-transformer";
+import { IsEnum, IsNumber, IsOptional, IsString } from "class-validator";
+
+export class PakageQueryDTO {
+    @ApiPropertyOptional({ description: 'Search by Pakage name' })
+    @IsOptional()
+    @IsString()
+    name?: string;
+
+    @ApiPropertyOptional({ description: 'Search by Price' })
+    @IsOptional()
+    @Type(() => Number)
+    @IsNumber()
+    price?: number;
+
+    @ApiPropertyOptional({ description: 'Filter by status (ACTIVE | INACTIVE)' })
+    @IsOptional()
+    @IsEnum(PackageStatus)
+    status?: PackageStatus;
+
+    @ApiPropertyOptional({ description: 'Sort field', example: 'createdAt' })
+    @IsOptional()
+    @IsString()
+    sortBy?: string;
+
+    @ApiPropertyOptional({ description: 'Sort order', example: 'asc' })
+    @IsOptional()
+    @IsString()
+    orderBy?: 'asc' | 'desc';
+
+    @ApiPropertyOptional({ description: 'Page number', example: 1 })
+    @IsOptional()
+    @Type(() => Number)
+    @IsNumber()
+    page?: number;
+
+    @ApiPropertyOptional({ description: 'Page size', example: 10 })
+    @IsOptional()
+    @Type(() => Number)
+    @IsNumber()
+    pageSize?: number;
+}
