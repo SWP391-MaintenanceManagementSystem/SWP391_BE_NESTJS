b4f61e2120da60418474c37edcf93ba1159a2475
Lamtailoi2 <lamtailoi11141@gmail.com>
2025-10-24 00:04:49 +0700

    [LoiLT] feat: update staff booking detail dto

--- src/modules/booking/dto/staff-booking-detail.dto.ts
+++ src/modules/booking/dto/staff-booking-detail.dto.ts
@@ -1,3 +1,39 @@
+import { Expose, Transform, Type } from 'class-transformer';
 import { CustomerBookingDetailDTO } from './customer-booking-detail.dto';
 
-export class StaffBookingDetailDTO extends CustomerBookingDetailDTO {}
+class ServiceCenterInfo {
+  @Expose()
+  id: string;
+
+  @Expose()
+  name: string;
+
+  @Expose()
+  address: string;
+}
+
+class TechnicianInfo {
+  @Expose()
+  id: string;
+  @Expose()
+  firstName: string;
+  @Expose()
+  lastName: string;
+}
+
+export class StaffBookingDetailDTO extends CustomerBookingDetailDTO {
+  @Expose()
+  @Type(() => ServiceCenterInfo)
+  serviceCenter: ServiceCenterInfo;
+  @Expose()
+  @Transform(
+    ({ obj }) =>
+      obj.bookingAssignments?.map((ba: any) => ({
+        id: ba.employee?.account.id,
+        firstName: ba.employee?.firstName,
+        lastName: ba.employee?.lastName,
+      })) || []
+  )
+  @Type(() => TechnicianInfo)
+  technicians: TechnicianInfo[];
+}
