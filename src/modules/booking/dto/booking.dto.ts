6f1afe0b63caa298ef5c6ee97dfc7ad5144f5f1f
lamtailoi2 <lamtailoi11141@gmail.com>
2025-11-06 16:58:15 +0700

    [LoiLT] feat: update customer's booking dto

--- src/modules/booking/dto/booking.dto.ts
+++ src/modules/booking/dto/booking.dto.ts
@@ -1,16 +1,53 @@
-import { Expose, Transform } from 'class-transformer';
+import { Expose, Transform, Type } from 'class-transformer';
 import { toZonedTime } from 'date-fns-tz';
 import { format } from 'date-fns/format';
 import { VN_DATE_TIME_FORMAT, VN_TIMEZONE } from 'src/common/constants';
+
+class VehicleInfo {
+  @Expose()
+  id: string;
+  @Expose()
+  licensePlate: string;
+  @Expose()
+  model: string;
+  @Expose()
+  brand: string;
+  @Expose()
+  productionYear: number;
+}
+
+class CenterInfo {
+  @Expose()
+  id: string;
+  @Expose()
+  name: string;
+}
+
 export class BookingDTO {
   @Expose()
   id: string;
   @Expose()
   customerId: string;
   @Expose()
-  vehicleId: string;
+  @Transform(({ obj }) => ({
+    id: obj.vehicle?.id,
+    licensePlate: obj.vehicle?.licensePlate,
+    vin: obj.vehicle?.vin,
+    model: obj.vehicle?.vehicleModel?.name,
+    brand: obj.vehicle?.vehicleModel?.brand?.name,
+    productionYear: obj.vehicle?.vehicleModel?.productionYear,
+  }))
+  @Type(() => VehicleInfo)
+  vehicle: VehicleInfo;
+
   @Expose()
-  centerId: string;
+  @Transform(({ obj }) => ({
+    id: obj.serviceCenter?.id,
+    name: obj.serviceCenter?.name,
+  }))
+  @Type(() => CenterInfo)
+  center: CenterInfo;
+
   @Expose()
   shiftId: string;
   @Expose()
