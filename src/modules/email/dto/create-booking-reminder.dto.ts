245e846a1a5789005744a9b79e42f9d7211a95c9
Lamtailoi2 <lamtailoi11141@gmail.com>
2025-11-06 23:48:46 +0700

    [LoiLT] feat: remind customer about their booking

---
+++ src/modules/email/dto/create-booking-reminder.dto.ts
@@ -0,0 +1,9 @@
+export class CreateBookingReminderDTO {
+  email: string;
+  username: string;
+  bookingDate: string;
+  bookingTime: string;
+  centerName: string;
+  location: string;
+  bookingDetailsURL: string;
+}
